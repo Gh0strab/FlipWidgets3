81e20f4d75e844b93dd28e8a57af9c303d9ec58d
Will White <60898498+Gh0strab@users.noreply.github.com>
2025-11-28 10:31:14 -0600

    Add files via upload

---
+++ app/src/main/java/com/flipcoverwidgets/app/FlipCoverWidgetsApp.kt
@@ -0,0 +1,25 @@
+package com.flipcoverwidgets.app
+
+import android.app.Application
+import com.flipcoverwidgets.app.data.AppDatabase
+import com.flipcoverwidgets.app.service.CoverScreenWidgetHostService
+import com.flipcoverwidgets.app.widget.WidgetHostManager
+
+class FlipCoverWidgetsApp : Application() {
+    
+    val database: AppDatabase by lazy {
+        AppDatabase.getDatabase(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        WidgetHostManager.startListening(this)
+        CoverScreenWidgetHostService.start(this)
+    }
+
+    companion object {
+        lateinit var instance: FlipCoverWidgetsApp
+            private set
+    }
+}
